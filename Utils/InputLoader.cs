85b0d91527f071ca505e23109f621f6b0f3b4921
Andreas Hackl <anhac@tricentis.com>
2021-12-02 15:33:29 +0100

    Add Day 1

---
+++ Utils/InputLoader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Utils
+{
+    public class InputLoader
+    {
+        public static IEnumerable<string> Load(int index = 0)
+        {
+            return File.ReadAllLines($"./Input/input{index}.txt");
+        }
+    }
+}
