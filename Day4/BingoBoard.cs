a5c32536ca1c5443ac3c58dac9419428b5856fbf
KuroScar <kuroscarlet@gmail.com>
2021-12-04 11:18:49 +0100

    Solve Day 4

---
+++ Day4/BingoBoard.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class BingoBoard
+{
+    private BingoNumber[][] BoardByRows { get; }
+    public BingoBoard(IEnumerable<string> lines)
+    {
+        BoardByRows = lines.Select(item => item.Split(" ", StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries)
+                                               .Select(num => new BingoNumber(num, false))
+                                               .ToArray())
+                           .ToArray();
+
+    }
+
+    public void NewNumber(string number)
+    {
+        BingoNumber bingoNumber = BoardByRows.SelectMany(item => item).FirstOrDefault(item => item.Number == number);
+        if (bingoNumber == null)
+        {
+            return;
+        }
+        bingoNumber.Marked = true;
+    }
+
+    public bool HasWon()
+    {
+        return BoardByRows.Concat(BoardByColumns)
+                          .Any(item => item.All(numbers => numbers.Marked));
+    }
+
+    public IEnumerable<string> GetAllUnmarkedNumbers => BoardByRows.SelectMany(item => item)
+                                                                   .Where(item => !item.Marked)
+                                                                   .Select(item => item.Number);
+    private IEnumerable<IEnumerable<BingoNumber>> BoardByColumns => Enumerable.Range(0, BoardByRows.First().Length)
+                                                                              .Select(index => BoardByRows.Select(row => row[index]));
+
+    record BingoNumber
+    {
+        public string Number { get; }
+
+        public bool Marked { get; set; }
+
+        public BingoNumber(string num, bool marked)
+        {
+            Number = num;
+            Marked = marked;
+        }
+    }
+}
---
+++ Day4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MoreLinq;
+
+using static Utils.InputLoader;
+
+List<string> lines = Load().Where(item => !string.IsNullOrWhiteSpace(item)).ToList();
+List<string> bingoNumbers = lines.First().Split(',').ToList();
+
+var bingoBoards = lines.Skip(1).Batch(5).Select(board => new BingoBoard(board)).ToList();
+
+
+SolvePart1(bingoNumbers, bingoBoards);
+SolvePart2(bingoNumbers, bingoBoards);
+
+
+void SolvePart2(List<string> list, List<BingoBoard> boards)
+{
+    foreach (string number in list)
+    {
+        boards.ForEach(board => board.NewNumber(number));
+        List<BingoBoard> wonBoards = boards.Where(item => item.HasWon()).ToList();
+        boards = boards.Except(wonBoards).ToList();
+        if (!boards.Any())
+        {
+            int sumUnmarkedNumbers = wonBoards.First().GetAllUnmarkedNumbers.Select(int.Parse).Sum();
+            Console.WriteLine($"{sumUnmarkedNumbers} * {number} = {sumUnmarkedNumbers * int.Parse(number)}");
+            return;
+        }
+    }
+}
+
+void SolvePart1(List<string> list, List<BingoBoard> bingoBoards1)
+{
+    foreach (string number in list)
+    {
+        bingoBoards1.ForEach(board => board.NewNumber(number));
+        BingoBoard wonBoard = bingoBoards1.FirstOrDefault(item => item.HasWon());
+
+        if (wonBoard != null)
+        {
+            int sumUnmarkedNumbers = wonBoard.GetAllUnmarkedNumbers.Select(int.Parse).Sum();
+            Console.WriteLine($"{sumUnmarkedNumbers} * {number} = {sumUnmarkedNumbers * int.Parse(number)}");
+            return;
+        }
+    }
+}
