d362b7dce0dc278b6d43fcd94a1a913be9cdaf62
KuroScar <kuroscarlet@gmail.com>
2021-12-07 12:51:14 +0100

    Day 7

---
+++ Day7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using static Utils.InputLoader;
+
+List<int> input = Load().First().Split(',').Select(int.Parse).ToList();
+
+int part1 = GetSumOfFuel(input, (location, target) => Math.Abs(location - target));
+Console.WriteLine($"Part1: {part1}");
+
+
+int part2 = GetSumOfFuel(input, (location, target) =>
+                                {
+                                    int distance = Math.Abs(location - target);
+                                    return (distance * (distance + 1)) / 2;
+                                });
+Console.WriteLine($"Part2: {part2}");
+
+int GetSumOfFuel(List<int> ints, Func<int, int, int> costs)
+{
+    int lastSum = int.MaxValue;
+    foreach (int i in Enumerable.Range(0, ints.Count))
+    {
+        int sum = ints.Sum(item => costs(item, i));
+        if (lastSum < sum)
+        {
+            return lastSum;
+        }
+        lastSum = sum;
+    }
+    return lastSum;
+}
