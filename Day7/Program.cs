0884d23d32aa23cfa0963f798439a91b8afda6fd
KuroScar <kuroscarlet@gmail.com>
2021-12-07 13:33:14 +0100

    Fix some issues with range

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -4,11 +4,10 @@ using System.Linq;
 
 using static Utils.InputLoader;
 
-List<int> input = Load().First().Split(',').Select(int.Parse).ToList();
+List<int> input = Load().First().Split(',').Select(int.Parse).OrderBy(item => item).ToList();
 
 int part1 = GetSumOfFuel(input, (location, target) => Math.Abs(location - target));
 Console.WriteLine($"Part1: {part1}");
-
 
 int part2 = GetSumOfFuel(input, (location, target) =>
                                 {
@@ -20,7 +19,7 @@ Console.WriteLine($"Part2: {part2}");
 int GetSumOfFuel(List<int> ints, Func<int, int, int> costs)
 {
     int lastSum = int.MaxValue;
-    foreach (int i in Enumerable.Range(0, ints.Count))
+    foreach (int i in Enumerable.Range(ints.First(), ints.Last()))
     {
         int sum = ints.Sum(item => costs(item, i));
         if (lastSum < sum)
